af4decfbe1f45ea9a668c25d754d076ab3a237d8
Pingpch <6640138622@student.chula.ac.th>
2023-12-03 12:17:14 +0700

    Update food-restaurant.py

--- food-restaurant.py
+++ food-restaurant.py
@@ -2,3 +2,16 @@ import streamlit as st
 import openai
 import json
 import pandas as pd 
+# Get the API key from the sidebar called OpenAI API key
+user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
+client = openai.OpenAI(api_key=user_api_key)
+
+prompt = """Create a 20 recommendation according to higher rating to lowest rating list of restaurants in 
+a place where clients request in the request box, rating score is from 5 to 1. And write a short review of the food, 
+according to real reviews. And then show the price range of each restaurant, switch and calculate the price from 
+any currency to USD and then calculate it to THB. And then show the location of the restaurant on the map by the following information:
+province, district, subdistrict, and street name. And then show the opening time and closing time of the restaurant.
+"""
+st.title("Food Restaurant")
+st.subheader("This is a food restaurant recommendation webpage")
+user_input = st.text_area("Enter some specific place that you want to find a restaurant", "Thailand, Bangkok")
