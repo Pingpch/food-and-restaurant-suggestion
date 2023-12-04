8f529a333c3b26b28dfb92df795a2e1fe97f3404
Pingpch <6640138622@student.chula.ac.th>
2023-12-04 22:57:57 +0700

    Update food-restaurant.py

--- food-restaurant.py
+++ food-restaurant.py
@@ -6,7 +6,7 @@ import streamlit_pandas as sp
 
 @st.cache(allow_output_mutation=True)
 def load_data():
-    df = pd.read_csv('data/food-restaurant.csv')
+    df = pd.read_csv('data/titanic.csv')
     return df
 
 df = load_data()
