0ec8e7386853606685edf0e288f923931b804075
Pingpch <6640138622@student.chula.ac.th>
2023-12-04 17:05:37 +0700

    Update food-restaurant.py

--- food-restaurant.py
+++ food-restaurant.py
@@ -15,14 +15,11 @@ List the suggestions in a JSON array, one suggestion per line.
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
 
 st.title("Food Restaurant Recommendation") 
 st.caption("A clean suggestion for easier way to find a restaurant")
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
